0df74b979412c801c36e4b95c572628fe588c81b
Rafael <rafael.acerqueirah@gmail.com>
2019-07-17 08:08:51 -0300

    Show MineField component on the App Component

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 import params from './src/params'
+import { createLandmineBoard } from './src/functions'
+import MineField from './src/components/Minefield'
 
 const App = () => {
+	const landMinesAmount = () => {
+		const cols = params.getColumnsAmount()
+		const rows = params.getRowsAmount()
+
+		return Math.ceil(cols * rows * params.difficultLevel)
+	}
+
+	const createState = () => {
+		const cols = params.getColumnsAmount()
+		const rows = params.getRowsAmount()
+		return {
+			board: createLandmineBoard(rows, cols, landMinesAmount)
+		}
+	}
+
+	const [board, setBoard] = useState(createState().board)
+
 	return (
-		<View>
+		<View style={styled.container}>
 			<Text>Starting minefield</Text>
 			<Text>
 				Grid size: {params.getRowsAmount()} x {params.getColumnsAmount()}
 			</Text>
+			<View style={styled.board}>
+				<MineField board={board} />
+			</View>
 		</View>
 	)
 }
+
+const styled = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'flex-end'
+	},
+	board: {
+		alignItems: 'center',
+		backgroundColor: '#AAA'
+	}
+})
 
 export default App
