790597ae7692dcd2fe09f8472dd1127de9c0596d
Rafael <rafael.acerqueirah@gmail.com>
2019-07-17 08:54:36 -0300

    Add new functions

--- src/functions.js
+++ src/functions.js
@@ -39,3 +39,64 @@ export const createLandmineBoard = (rows, columns, landMinesAmount) => {
 	spreadMines(board, landMinesAmount)
 	return board
 }
+
+export const cloneBoard = board => {
+	return board.map(rows => {
+		return rows.map(field => ({ ...field }))
+	})
+}
+
+const getNeighbors = (board, row, column) => {
+	const neighbors = []
+	const rows = [row - 1, row, row + 1]
+	const columns = [column - 1, column, column + 1]
+	rows.forEach(r => {
+		columns.forEach(c => {
+			const different = r !== row || c !== column
+			const validRow = r >= 0 && r < board.length
+			const validColumn = c >= 0 && c < board[0].length
+			if (different && validRow && validColumn) {
+				neighbors.push(board[r][c])
+			}
+		})
+	})
+	return neighbors
+}
+
+const safeNeighborhood = (board, row, column) => {
+	const safes = (result, neighbor) => result && !neighbor.mined
+	return getNeighbors(board, row, column).reduce(safes, true)
+}
+
+export const openField = (board, row, column) => {
+	const field = board[row][column]
+	if (!field.opened) {
+		field.opened = true
+		if (field.mined) {
+			field.exploded = true
+		} else if (safeNeighborhood(board, row, column)) {
+			getNeighbors(board, row, column).forEach(n =>
+				openField(board, n.row, n.column)
+			)
+		} else {
+			const neighbors = getNeighbors(board, row, column)
+			field.nearMines = neighbors.filter(n => n.mined).length
+		}
+	}
+}
+
+const fields = board => [].concat(...board)
+
+export const hasExplosion = board =>
+	fields(board).filter(field => field.exploded).length > 0
+
+const pending = field =>
+	(field.mined && !field.flagged) || (!field.mined && !field.opened)
+
+export const wonGame = board =>
+	fields(board).filter(field => field.pending).length === 0
+
+export const showLandMines = board =>
+	fields(board)
+		.filter(field => field.mined)
+		.forEach(field => (field.opened = true))
